3ad828c51959d6513bed832d5980a356d064cd00
fellipecastro7 <fellipemachado77@gmail.com>
2023-04-02 14:43:30 -0300

    wip: center square

--- src/tests/bgrabitmap_square/unit1.pas
+++ src/tests/bgrabitmap_square/unit1.pas
@@ -13,16 +13,16 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
-    bmp : TBGRABitmap;
     Button1: TButton;
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   private
-
+    FBitmap : TBGRABitmap;
+    //CenterWidth : integer;
+    //CenterHeight : integer;
   protected
     procedure Paint; override;
-
   public
 
   end;
@@ -38,8 +38,9 @@ implementation
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  Width := Screen.Width;
-  Height := Screen.Height;
+  FBitmap := TBGRABitmap.Create(ClientWidth, ClientHeight);
+  //CenterWidth := ClientWidth div 2;
+  //CenterHeight := ClientHeight div 2;
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -48,17 +49,30 @@ begin
 end;
 
 procedure TForm1.Paint;
+var
+  R : TRect;
+  CenterWidth : integer;
+  CenterHeight: integer;
+  SquareWidth: integer;
+  SquareHeight: integer;
+  SquareX : integer;
+  SquareY : integer;
 begin
   inherited Paint;
-  bmp := TBGRABitmap.Create(Width, Height);
-  bmp.Fill(VGAWhite);
-  bmp.FillRectAntialias(300, 200, 100, 100, BGRA(255, 0, 0, 255));
-  bmp.Draw(Canvas, 0, 0, True);
+  CenterWidth := ClientWidth div 2;
+  CenterHeight := ClientHeight div 2;
+  SquareWidth := 150;
+  SquareHeight := 150;
+  SquareX := CenterWidth - SquareWidth div 2;
+  SquareY := CenterHeight - SquareHeight div 2;
+  R := Rect(SquareX, SquareY, SquareX + SquareWidth, SquareY + SquareHeight);
+  FBitmap.Fill(VGARed);
+  FBitmap.Draw(Canvas, R);
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
-  bmp.Free;
+  FBitmap.Free;
 end;
 
 end.
